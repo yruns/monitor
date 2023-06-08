4c4b80633dc093a96a19f8b3ef6b3116819f41f8
yruns <yruns.sh@qq.com>
2023-06-08 23:23:34 +0800

    fix log

--- model/log.go
+++ model/log.go
@@ -5,6 +5,7 @@ import "time"
 type Log struct {
 	Id   uint      `json:"id" form:"id" gorm:"primary_key"`
 	Info string    `json:"info" form:"info"`
-	From string    `json:"from" form:"to"`
+	From string    `json:"from" form:"from"`
+	To   string    `json:"to" form:"to"`
 	Date time.Time `json:"date" form:"time"`
 }
